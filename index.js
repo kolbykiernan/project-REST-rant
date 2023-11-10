7ea271d41a3f2faed20d6e518f30d5b604dbf78e
Kolby Kiernan <kolbykiernan@gmail.com>
2023-11-09 19:37:30 -0800

    Adding home page view

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//<------------defining JSX---------------------->//
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
 //<------------connecting our router for places-------------------->//
-app.use('/places', require('./controller/places'))
+app.use('/places', require('./controllers/places'))
 
 //<------------Home page?-------------------->//
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('home')
 })
 
 //<------------Adding 404 page route------------->//
---
+++ views/default.jsx
@@ -0,0 +1,20 @@
+//<-------------Installing React and Express React Views------------------->//
+
+const React = require('react')
+
+//<---Create function that will allow us to pass html as argument instead of rewriting. JSX--------->//
+function Def (html) {
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
+}
+
+///<-------Export the function, not exactly sure ------------>//
+module.exports = Def
