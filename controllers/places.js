ca7a7fb518bdc1d7d27584ae6dcd6ab6453c2308
Kolby Kiernan <kolbykiernan@gmail.com>
2023-11-16 19:03:13 -0800

    Bonus Part 2, State Dropdown

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,11 @@
 //<-------------------creating a router------------------->//
 const router = require('express').Router()
+const places = require('../models/places.js')
+
+//<------------ GETting places---------------------->//
+router.get('/', (req, res) =>{
+  res.render('places/index', {places})
+})
 
 router.get('/new', (req, res) => {
     res.render('places/new')
@@ -8,28 +14,20 @@ router.get('/new', (req, res) => {
 
   router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
+  if(!req.body.pic){
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if(!req.body.city){
+    req.body.city = 'Anytown'
+  }
+  if(!req.body.state){
+    req.body.state = 'USA'
+  }
+    places.push(req.body)
+    res.redirect('/places')
   })
   
 
-//<------------ GETting places---------------------->//
-router.get('/', (req, res) =>{
 
-    let places = [{
-            name: 'H-Thai-ML',
-            city: 'Seattle',
-            state: 'WA',
-            cuisines: 'Thai, Pan-Asian',
-            pic: '/images/thai.jpg',
-}, {
-            name: 'Coding Cat Cafe',
-            city: 'Phoenix',
-            state: 'AZ',
-            cuisines: 'Coffee, Bakery',
-            pic: '/images/cafe.jpg' 
-    }]
-
-    res.render('places/index', {places})
-})
 
 module.exports = router
---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/thai.jpg',
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/cafe.jpg' 
+}]
