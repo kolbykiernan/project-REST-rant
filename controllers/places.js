f64aef614f52ce9bc938b81d47abfef11e739ac3
Kolby Kiernan <kolbykiernan@gmail.com>
2023-11-19 15:14:02 -0800

    added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -7,10 +7,14 @@ router.get('/', (req, res) =>{
   res.render('places/index', {places})
 })
 
+
+//<------Takes user to page where they can add a new place----->./
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
   
+
+//<---------Takes user to list of places page------->//
 router.post('/', (req, res) => {
     console.log(req.body)
   if(!req.body.pic){
@@ -26,6 +30,7 @@ router.post('/', (req, res) => {
     res.redirect('/places')
 })
   
+//<----------Takes user from places page to individual restaurant----->
 router.get('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
@@ -39,6 +44,7 @@ router.get('/:id', (req, res) => {
       }
 })
 
+//<--------Lets user delete a restaurant-------------->//
 router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -52,6 +58,23 @@ router.delete('/:id', (req, res) => {
     res.send('STUB DELETE places/:id')
   }
 })
+
+//<-----------Let's user edit a rtestaurant------------>//
+
+router.get('/:id/edit', (req, res) => {
+
+  let id=Number(req.params.id)
+  if (isNaN(id)){
+    res.render('error404')
+  } else if 
+    (!places[id]) {
+      res.render('error404')
+    } else{
+      res.render('places/edit', {place: places[id]})
+    }
+})
+
+
 
 
 
