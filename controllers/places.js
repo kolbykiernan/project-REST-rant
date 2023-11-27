8e4f8fc094d90c8a86a3e6c8b020ab3b8be23a24
Kolby Kiernan <kolbykiernan@gmail.com>
2023-11-26 17:08:27 -0800

    linked places and show page to mongoose

--- controllers/places.js
+++ controllers/places.js
@@ -1,110 +1,61 @@
-//<-------------------creating a router------------------->//
 const router = require('express').Router()
-const places = require('../models/places.js')
+const db = require('../models')
 
-//<------------ GETting places---------------------->//
-router.get('/', (req, res) =>{
-  res.render('places/index', {places})
+router.get('/', (req, res) => {
+  db.Place.find()
+  .then(places => {
+    res.render('places/index', {places})
+  })
+  .catch(err => {
+    console.log(err)
+    res.render('error404')
+  })
 })
 
-
-//<------Takes user to page where they can add a new place----->./
-router.get('/new', (req, res) => {
-    res.render('places/new')
-})
-  
-  
-//<----------Takes user from places page to individual restaurant----->
-router.get('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-      res.render('error404')
-    }
-    else if(!places[id]) {
-      res.render('error404')
-    }
-    else{
-        res.render('places/show', { place: places[id], id })
-      }
-})
-
-//<---------Receives data and takes user to list of places page------->//
 router.post('/', (req, res) => {
-  console.log(req.body)
-if(!req.body.pic){
-  req.body.pic = 'http://placekitten.com/400/400'
-}
-if(!req.body.city){
-  req.body.city = 'Anytown'
-}
-if(!req.body.state){
-  req.body.state = 'USA'
-}
-  places.push(req.body)
-  res.redirect('/places')
+  db.Place.create(req.body)
+  .then(() => {
+    res.redirect('/places')
+  })
+  .catch(err => {
+    console.log('err', err)
+    res.render('error404')
+  })
 })
 
-//<-----------submits the edited restaurant-------------->//
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+  db.Place.findById(req.params.id)
+  .then(place => {
+    res.render('places/show', {place})
+  })
+  .catch(err => {
+    console.log('err', err)
+    res.render('error404')
+  })
+})
 
 router.put('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    if (!req.body.pic){
-      req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if (!req.body.city){
-      req.body.city = 'Anytown'
-    }
-    if (!req.body.state){
-      req.body.state = 'USA'
-    }
-      places[id] = req.body
-      res.redirect(`/places/${id}`)
-  }
+  res.send('PUT /places/:id stub')
 })
 
-//<--------Lets user delete a restaurant-------------->//
 router.delete('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.redirect('error404')
-  }
-  else {
-    places.splice(id, 1)
-    res.redirect('/places')
-  }
+  res.send('DELETE /places/:id stub')
 })
-
-//<-----------Let's user edit a restaurant------------>//
 
 router.get('/:id/edit', (req, res) => {
-
-  let id=Number(req.params.id)
-
-
-  if (isNaN(id)){
-    res.render('error404')
-  } else if (!places[id]) {
-      res.render('error404')
-    } else {
-      res.render('places/edit', {
-        place: places[req.params.id],
-        index: req.params.id
-      })
-    }
+  res.send('GET edit form stub')
 })
 
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
 
-
-
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
 
 module.exports = router
--- models/index.js
+++ models/index.js
@@ -1,17 +1,11 @@
 //<-------connecting to mongoose----------->//
 const mongoose = require('mongoose')
-const placeSchema = new mongoose.Schema({
-  name: {type: String, required: true},
-  pic: String,
-  cuisines: {type: String, required: true},
-  city: {type: String, default: 'Anytown'},
-  state: {type: String, default: 'USA'},
-  founded: Number
-})
-
-
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, 
   useUnifiedTopology: true
 })
+
+module.exports.Place = require('./places')
+
+console.log(module.exports)
