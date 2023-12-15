9219b0a5b5d4cbc9a334b997ed5c2ad6392aeedb
kolbykiernan <142462985+kolbykiernan@users.noreply.github.com>
2023-12-14 18:06:26 -0800

    added validation error functionality

--- controllers/places.js
+++ controllers/places.js
@@ -22,8 +22,17 @@ router.post('/', (req, res) => {
     res.redirect('/places')
   })
   .catch(err => {
-    console.log('err', err)
-    res.render('error404')
+    if (err && err.name == 'ValidationError') {
+      let message = 'Validation Error: '
+      for (let field in err.errors) {
+       message+= `${field} was ${err.errors[field].value}. `
+       message+= `${err.errors[field].message}`
+      }
+      console.log('Validation error message', message)
+      res.render('places/new', {message})
+    } else {
+      res.render('error404')
+    }
   })
 })
 
